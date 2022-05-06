311ac70cb1a39081c0b99f221105ed0b74eb3998
Nick <65818641+CreepCrafter04@users.noreply.github.com>
2022-05-06 23:06:41 +0200

    [web-ui] added unsupported viewport height overlay

--- webui-src/web-ui/dashboard/assets/script.js
+++ webui-src/web-ui/dashboard/assets/script.js
@@ -1,5 +1,21 @@
 // NeoLight Dashboard Script: nl-ui-v0.1 //
 
+minHeight = 600;
+
 setInterval(()=>{
-    document.getElementById("test").innerHTML = visualViewport.width;
+    document.getElementById("test").innerHTML = Math.floor(visualViewport.width);
 },100)
+
+window.onresize = checkVpHeight;
+window.onload = checkVpHeight;
+
+function checkVpHeight(){
+    if(visualViewport.height < minHeight){
+        document.getElementById("app-overlay").style.display = "flex";
+        document.getElementById("app-overlay-req").innerText = "width: any (300+ recommended), height: "+minHeight+"+";
+        document.getElementById("app-overlay-cur").innerText = "width: "+Math.floor(visualViewport.width)+", height: "+visualViewport.height;
+    }
+    else{
+        document.getElementById("app-overlay").style.display = ""
+    }
+}
